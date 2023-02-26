8e0ded8447b6d92de5bf1d363706571e572ccb87
mulmuri <68294539+mulmuri@users.noreply.github.com>
2023-02-26 16:19:54 +0900

    Write IjddcApplicationTests.java

---
+++ src/test/java/uos/urbanscience/ijddc/IjddcApplicationTests.java
@@ -0,0 +1,13 @@
+package uos.urbanscience.ijddc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IjddcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
