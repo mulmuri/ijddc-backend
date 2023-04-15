c8a93993501ab413620d359085d2f8abbc46ad1b
mulmuri <68294539+mulmuri@users.noreply.github.com>
2023-04-15 14:01:31 +0900

    Write UserInfoRepository.java

---
+++ src/main/java/uos/urbanscience/ijddc/infrastructure/database/repository/UserInfoRepository.java
@@ -0,0 +1,10 @@
+package uos.urbanscience.ijddc.infrastructure.database.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import uos.urbanscience.ijddc.infrastructure.database.entity.UserInfo;
+
+@Repository
+public interface UserInfoRepository extends JpaRepository<UserInfo, Long> {
+
+}
