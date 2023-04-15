b6404adccf599ebe6407a45a2d3df7397c77d882
mulmuri <68294539+mulmuri@users.noreply.github.com>
2023-04-15 14:01:31 +0900

    Write UserEmailRepository.java

---
+++ src/main/java/uos/urbanscience/ijddc/infrastructure/database/repository/UserEmailRepository.java
@@ -0,0 +1,10 @@
+package uos.urbanscience.ijddc.infrastructure.database.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import uos.urbanscience.ijddc.infrastructure.database.entity.UserEmail;
+
+@Repository
+public interface UserEmailRepository extends JpaRepository<UserEmail, String> {
+
+}
