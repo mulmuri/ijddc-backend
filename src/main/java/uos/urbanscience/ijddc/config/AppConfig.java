08a9e8cefedd28f6cf7b753a47d51da6ea7e8d09
mulmuri <68294539+mulmuri@users.noreply.github.com>
2023-04-18 02:38:03 +0900

    Write AppConfig.java

---
+++ src/main/java/uos/urbanscience/ijddc/config/AppConfig.java
@@ -0,0 +1,17 @@
+package uos.urbanscience.ijddc.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+
+@Configuration
+@ComponentScan(basePackages = "com.example")
+public class AppConfig {
+
+}
