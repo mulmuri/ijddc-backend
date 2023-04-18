ba84602258a50a1e268d8be93e04ecf0cd5ea67a
mulmuri <68294539+mulmuri@users.noreply.github.com>
2023-04-19 01:05:57 +0900

    Write AccessTokenService.java

---
+++ src/main/java/uos/urbanscience/ijddc/domain/service/token/AccessTokenService.java
@@ -0,0 +1,63 @@
+package uos.urbanscience.ijddc.domain.service.token;
+
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import uos.urbanscience.ijddc.domain.value.user.User;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+
+@Component
+public class AccessTokenService extends TokenServiceAbstract {
+
+    @Value("${ACCESS_KEY}")
+    private String secret;
+
+    @Override
+    public String generateToken(User user) {
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("userid", user.getUserId());
+        claims.put("role", user.getUserRole().ToCode());
+        return createToken(claims, Long.toString(user.getUserId()));
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            return extractExpiration(token).before(new Date());
+        } catch (JwtException e) {
+            return false;
+        }
+    }
+
+    public User extractUser(String token) {
+        Long userId = extractUserId(token);
+        int role = extractUserRole(token);
+        return new User(userId, role);
+    }
+
+    private Long extractUserId(String token) {
+        return extractClaim(token, claims -> claims.get("userid", Long.class));
+    }
+
+    private int extractUserRole(String token) {
+        return extractClaim(token, claims -> claims.get("role", int.class));
+    }
+
+    private Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+
+    private  <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        return super.extractClaim(token, claimsResolver, secret);
+    }
+
+    private String createToken(Map<String, Object> claims, String subject) {
+        return super.createToken(claims, subject, secret);
+    }
+}
