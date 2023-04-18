fb9221f44ea785e357614b1bd6692b4aaf1601e7
mulmuri <68294539+mulmuri@users.noreply.github.com>
2023-04-18 22:42:06 +0900

    Write PaperStatusService.java

---
+++ src/main/java/uos/urbanscience/ijddc/domain/service/paper/PaperStatusService.java
@@ -0,0 +1,19 @@
+package uos.urbanscience.ijddc.domain.service.paper;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import uos.urbanscience.ijddc.domain.value.policy.PaperStatus;
+
+public class PaperStatusService {
+
+    private final PaperS
+
+    @Autowired
+
+
+    public PaperStatus getPaperStatus(long paperId) {
+    }
+
+    public void setPaperStatus(long paperId, PaperStatus status) {
+
+    }
+}
