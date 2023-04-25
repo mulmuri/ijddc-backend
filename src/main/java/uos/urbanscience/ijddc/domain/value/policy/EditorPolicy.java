504817de614af8c77440a7328ffae0f1367c0136
mulmuri <68294539+mulmuri@users.noreply.github.com>
2023-04-25 19:49:28 +0900

    Write EditorPolicy.java

---
+++ src/main/java/uos/urbanscience/ijddc/domain/value/policy/EditorPolicy.java
@@ -0,0 +1,55 @@
+package uos.urbanscience.ijddc.domain.value.policy;
+
+import uos.urbanscience.ijddc.exception.service.UnknownEnumException;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class EditorPolicy {
+
+    private static List<PaperStatus> getAll() {
+        return new ArrayList<PaperStatus> (Arrays.asList(
+                PaperStatus.submitted,
+                PaperStatus.denied,
+                PaperStatus.assigned,
+                PaperStatus.reviewed,
+                PaperStatus.polishing,
+                PaperStatus.confirmed,
+                PaperStatus.rejected,
+                PaperStatus.issued
+        ));
+    }
+
+    private static List<PaperStatus> getWaiting() {
+        return new ArrayList<PaperStatus> (Arrays.asList(
+                PaperStatus.assigned,
+                PaperStatus.polishing
+        ));
+    }
+
+    private static List<PaperStatus> getActionRequired() {
+        return new ArrayList<PaperStatus> (Arrays.asList(
+                PaperStatus.submitted,
+                PaperStatus.reviewed,
+                PaperStatus.confirmed
+        ));
+    }
+
+    private static List<PaperStatus> getFinished() {
+        return new ArrayList<PaperStatus> (Arrays.asList(
+                PaperStatus.rejected,
+                PaperStatus.issued
+        ));
+    }
+
+    protected static List<PaperStatus> getByGroupStatus(PaperGroupStatus status) {
+        return switch (status) {
+            case actionRequired -> getActionRequired();
+            case finished -> getFinished();
+            case waiting -> getWaiting();
+            case all -> getAll();
+            default -> throw new UnknownEnumException(status);
+        };
+    }
+}
