0626c4e3f6e31f8ae397258d22b9c521bd0d6c24
mulmuri <68294539+mulmuri@users.noreply.github.com>
2023-04-25 19:56:09 +0900

    Write AccountUseCase.java

---
+++ src/main/java/uos/urbanscience/ijddc/domain/usecase/account/AccountUseCase.java
@@ -0,0 +1,25 @@
+package uos.urbanscience.ijddc.domain.usecase.account;
+
+import uos.urbanscience.ijddc.domain.port.in.AccountPort;
+import uos.urbanscience.ijddc.domain.value.user.UserAccount;
+import uos.urbanscience.ijddc.domain.value.user.UserRole;
+
+import java.util.List;
+
+public class AccountUseCase implements AccountPort {
+
+    public UserAccount getUserAccount(long userId) {
+
+    }
+
+    public List<UserAccount> getALlUserAccount() {
+
+    }
+
+    public void updateUserAccount(long userId, UserAccount account) {
+
+    }
+    public void updateUserRole(long userId, UserRole role, boolean flag) {
+
+    }
+}
