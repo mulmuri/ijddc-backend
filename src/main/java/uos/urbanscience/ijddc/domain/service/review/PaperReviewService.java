eb65112b28cf7161a596f96e8bbf528c284616a1
mulmuri <68294539+mulmuri@users.noreply.github.com>
2023-04-25 19:25:43 +0900

    Write PaperReviewService.java

---
+++ src/main/java/uos/urbanscience/ijddc/domain/service/review/PaperReviewService.java
@@ -0,0 +1,18 @@
+package uos.urbanscience.ijddc.domain.service.review;
+
+import uos.urbanscience.ijddc.domain.value.review.Review;
+
+public class PaperReviewService {
+
+    public void assignPaperReviewToReviewer(long paperId, long reviewerId) {
+
+    }
+
+    public void submitReview(long reviewId, long paperId, Review review) {
+
+    }
+
+    public boolean checkAllReviewSubmitted(long paperId) {
+
+    }
+}
